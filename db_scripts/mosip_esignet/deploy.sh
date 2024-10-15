08e327b267984984de54fb33789441cbf4be2172
Tejash JL <tejash.jl1020@gmail.com>
2024-10-15 19:10:15 +0530

    Update deploy.sh

--- db_scripts/mosip_esignet/deploy.sh
+++ db_scripts/mosip_esignet/deploy.sh
@@ -1,7 +1,8 @@
 
 ## Properties file
 set -e
-
+env
+echo $SU_USER
 ## Grants
 PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -f init.sql
 
